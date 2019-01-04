1100b317bbfdedc31897fac0003cecf157c7c613
sainsh <kfsainsh@gmail.com>
2019-01-04 09:46:47 +0100

    Start of project

---
+++ src/main/java/GameProjectApp.java
@@ -0,0 +1,15 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class GameProjectApp extends GameApplication {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+}
